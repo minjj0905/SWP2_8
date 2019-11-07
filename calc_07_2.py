8c965baa134e0f0aa92dfa363e335a18d34b91ef
Suni0613 <kmj296744@daum.net>
2019-11-07 11:11:26 +0900

    10진수 로마자로 변환 구현

--- calc_07_2.py
+++ calc_07_2.py
@@ -90,6 +90,10 @@ class Calculator(QWidget):
 
         if key in operatorList :
             self.isDotUsed = False
+        elif key in functionList:
+            n = self.display.text()
+            value = functionMap[functionList.index(key)][1](n)
+            self.display.setText(str(value))
         if key == '=':
             try:
                 self.isDotUsed = False
