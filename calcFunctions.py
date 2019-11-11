fc8fefce9acd5a62f58b1f5218c57800c6194290
Suni0613 <kmj296744@daum.net>
2019-11-11 17:25:30 +0900

    파일 병합

--- calcFunctions.py
+++ calcFunctions.py
@@ -7,10 +7,8 @@ def factorial(numStr):
 
     return num
 
-
 def decToBin(numStr):
     return format(int(numStr), 'b')
-
 
 def binToDec(numStr):
     return int(numStr, 2)
@@ -38,3 +36,14 @@ def decToRoman(numStr):
 
     return result
 
+def romanToDec(numStr):
+    Numdic = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+    num = 0
+    L = list(numStr)
+    for i in range(1, len(L)):
+        if Numdic[L[i-1]] >= Numdic[L[i]]:
+            num += Numdic[L[i-1]]
+        else:
+            num -= Numdic[L[i-1]]
+    num += Numdic[L[-1]]
+    return num
