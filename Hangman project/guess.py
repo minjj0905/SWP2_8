e6f9891c9ede535d3995c03e03acfae284dd2f30
KooHyongMo <chrisais9@naver.com>
2019-11-26 10:38:07 +0900

    initialize Hangman Project

---
+++ Hangman project/guess.py
@@ -0,0 +1,37 @@
+class Guess:
+    
+    def __init__(self, word):
+        self.word = word
+        self.guessedChars = []
+        self.numTries = 0
+        self.foundChars = []
+
+    def display(self):
+        print('\n')
+        self.currentList = ["▢"]*len(self.word)
+        for i, j in self.foundChars:
+            self.currentList[j] = i
+        print(" ".join(self.currentList))
+
+    def guess(self, character):
+        if character not in self.word:
+            self.numTries += 1
+        if character not in self.guessedChars:
+            self.guessedChars.append(character)
+            for i in range(len(self.word)):
+                if character == self.word[i]:
+                    self.foundChars.append((character, i))
+        if len(self.foundChars) == len(self.word):
+            return True
+        else:
+            return False
+
+
+if __name__ == '__main__':
+    G = Guess('apple')
+    while True:
+        char = input("Select a letter: ")
+        if G.guess(char):
+            break
+        G.display()
+    G.display()
